115eea33ffacd8c1ebeb64e9f1c69c0cb4b5a45d
AffanHussain15 <affanhussain015@gmail.com>
2024-12-29 16:40:49 +0500

    Add files via upload

--- Class/script.js
+++ Class/script.js
@@ -1,73 +1,124 @@
-// // *** Reset Operators ***
+// function reverseWords(a) {
+//   //   let makeArr = a.split(" ");
+//   //   let reverseWordIntoArr = makeArr.map((item, index) => {
+//   //     return item.split("").reverse().join("");
+//   //   });
 
-// function restOperator() {
-//   let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,];
-//   let even = [];
-//   for (let i = 0; i < array.length; i++) {
-//       if (0 === array[i] % 10) {
-//           even.push(array[i]);
-//         }
-//     }
-//     console.log(even);
-// }
-// restOperator();
-
-// // Math.max(array)
-// // Math.max(array[i])
-// // ...Affan
-// // return
-// // restOperator(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
-// // console.log(Affan);
-// // console.log(restOperator);
-
-// // *** Spread Operator ***
-
-// const arr1 = [1,2,3];
-// const arr2 = [...arr1 , 4,5,6];
-// const arr3 = [...arr2 , 7,8,9];
-// const arr4 = [...arr3 , 10,11,12];
-// const arr5 = [...arr4 , 13,14,15];
-// const arr6 = [...arr5 , 16,17,18];
-// const arr7 = [...arr6 , 19,20,21];
-// const arr8 = [...arr7 , 22,23,24];
-
-// let newarr = arr1 +" "+ arr2;
-
-// console.log(arr8);
-
-// // *** Enhance Object Literals ***
-
-// const naam = "Affan";
-// const umar = 12;
-// let obj = {
-//   naam,
-//   umar,
-// };
-
-// console.log(obj);
-
-// const person = {
-//     name: 'Affan',
-//     greet(){
-//         console.log(`Hello, My name is ${this.name}.`);
-//     }
+//   //   let arrayConvertIntoWord = reverseWordIntoArr.join(" ");
+//   //   return arrayConvertIntoWord;
+//   return a
+//     .split(" ")
+//     .map((item) => item.split("").reverse().join(""))
+//     .join(" ");
 // }
 
-// person.greet();
+// function arr(){
+//     let make = a.split(" ");
+//     let reverse = make.map((item, index) => {
+//         return item.split("").reverse().join("");
+//     });
+//     console.log(a);
+// }
 
-// const key = "Hobby";
-
-// const personc = {
-//   name: "Affan",
-//   [key]: "Cricket",
+// function kaamPuraHua(message){
+//     console.log("Call back Function chal gaya " + message );
 // };
 
-// console.log(personc.Hobby);
+// kaamPuraHua();
+
+// function kaamkaro(callBack){
+//     callBack("Yeh hai callback ka message.");
+//     // console.log("kaam shuru ho gaya");
+//     // setTimeout(() => {
+//         // console.log("kaam pura ho gaya");
+//     // }, 1000);
+// };
+
+// kaamkaro(kaamPuraHua);
+
+// function one(){
+//     console.log("first");
+// };
+
+// function two(){
+//     console.log("second");
+// };
+
+// one();
+// two();
+
+// function countDown(message){
+//     console.log("countDown " + message);
+// }
+
+// function secCountDown(callBack){
+//     setTimeout(() => {
+//         console.log(1);
+//         setTimeout(() =>{
+//             console.log(2);
+//             setTimeout(() =>{
+//                 console.log(3);
+//                 setTimeout(() =>{
+//                     console.log(4);
+//                     setTimeout(() =>{
+//                         console.log(5);
+//                         setTimeout(() =>{
+//                             callBack("Completed");
+//                         }, 1000)
+//                     }, 1000)
+//                 }, 1000)
+//             }, 1000)
+//         }, 1000)
+//     }, 1000);
+
+// }
+
+// secCountDown(countDown)
+
+// function countDown() {
+//   let arr = [5,4,3,2,1];
+//   for (let i = 5; i >= 0; i--) {
+//     console.log(i);
+//   }
+// }
+// countDown();
+
+// console.log(countDown);
+
+// function cb(message) {
+//   console.log(message);
+// };
+
+// function countDown(timer, callBack) {
+//   if (timer > 0) {
+//     setTimeout(() => {
+//       console.log(timer);
+//       countDown(timer - 1, callBack);
+//     }, 500);
+//   } else {
+//     setTimeout(() => {
+//       callBack("CountDown Completed!");
+//     }, 500);
+//   }
+// };
+
+// countDown(5, cb);
 
 
-// *** Concat ***
+let map = new Map();
 
-let concatArr1 = [1,2,3,4];
-let concatArr2 = [5,6,7]
-let mergeArray = concatArr1.concat(concatArr2,8,9);
-console.log(mergeArray);
+let obj1 = {id: 1};
+let obj2 = {id: 2};
+
+
+map.set(obj1,"object 1");
+map.set(obj2,"object 2");
+
+function multiplyby(x){
+  return function(y){
+    return x * y
+  };
+}
+
+const multiplyby2 = multiplyby(2);
+console.log(multiplyby2(5));
